bc27e43f6fdd3ef7314210139413c4128e5693d4
Kelly Wachtel <kwachtel24@gmail.com>
2018-10-28 22:25:46 -0500

    add sorting assignment to repo

--- 04/sorting.rkt
+++ 04/sorting.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-reader.ss" "lang")((modname sorting) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 #|
 
 Here is an algorithm for sorting lists of numbers
@@ -33,3 +30,98 @@ or structural recursion. Also write down the running
 time of each function using Big Oh notation.
 
 |#
+
+;; A [Listof Number] is either:
+;; - '()
+;; - (cons Number [Listof Number]
+
+;; O(nlogn)
+;; merge-sort : [Listof Number] -> [Listof Number]
+;; given a list of numbers, return the list in ascending order
+;; gererative recursion
+(define (merge-sort a-list)
+  (cond
+    [(or (empty? a-list) (= (length a-list) 1)) a-list]
+    [else
+     [local [(define my-split-list (split-list a-list))] ;; (length of a-list)/2
+       (merge-two-lists
+        (make-posn
+         (merge-sort (posn-x my-split-list))
+         (merge-sort (posn-y my-split-list))))]]))
+
+;; Examples
+(check-expect (merge-sort '()) '())
+(check-expect (merge-sort '(1)) '(1))
+(check-expect (merge-sort '(1 2 3 4)) '(1 2 3 4))
+(check-expect (merge-sort '(4 3 2 1)) '(1 2 3 4))
+(check-expect (merge-sort '(1 1 1)) '(1 1 1))
+(check-expect (merge-sort '(1 2 1 2)) '(1 1 2 2))
+(check-expect (merge-sort '(1 6 3 5 7 3 2)) '(1 2 3 3 5 6 7))
+
+;; merge-two-lists: [Listof Number] [Listof Number] -> [Listof Number]
+;; assemble two sorted lists into one sorted in ascending order
+;; strategy: structural decomp. and structural recursion
+;;O(n)
+(define (merge-two-lists a-posn)
+  (cond
+    [(empty? (posn-x a-posn)) (posn-y a-posn)] ;;1
+    [(empty? (posn-y a-posn)) (posn-x a-posn)] ;;1
+    [else
+     (cond
+       [(< (first (posn-x a-posn)) (first (posn-y a-posn))) ;;1 + 1 + 1
+        (cons (first (posn-x a-posn)) (merge-two-lists (make-posn
+                                                        (rest (posn-x a-posn))
+                                                        (posn-y a-posn))))]
+       [else 
+        (cons (first (posn-y a-posn)) (merge-two-lists (make-posn
+                                                        (posn-x a-posn)
+                                                        (rest (posn-y a-posn)))))])]))
+
+;; Examples
+(check-expect (merge-two-lists (make-posn '() '())) '())
+(check-expect (merge-two-lists (make-posn (list 1) '())) (list 1))
+(check-expect (merge-two-lists (make-posn (list 2) (list 1))) (list 1 2))
+(check-expect (merge-two-lists (make-posn (list 3 5) (list 1 4))) (list 1 3 4 5))
+(check-expect (merge-two-lists (make-posn (list 3 5 6 7) (list 1 4))) (list 1 3 4 5 6 7))
+(check-expect (merge-two-lists (make-posn (list 3 5) (list 1 1 4))) (list 1 1 3 4 5))
+
+
+
+; split-list: [Listof Number]-> (make-posn [Listof Number] [Listof Number])
+; given a list of numbers, splits it into two lists of numbers
+; structural decomp. and functional comp.
+;; O(n/2)
+(define (split-list a-list)
+  (local [;; split-list: [Listof Number] [Listof Number][empty] Number
+          ;; -> (make-posn [Listof Number] [Listof Number])
+          ;; given a list and an integer < length of the list,
+          ;; return the a list containing the first n numbers of that list
+          (define (split-list start-list end-list n)
+            (cond
+              [(empty? start-list) (make-posn start-list end-list)]
+              [(zero? n) (make-posn start-list end-list)]
+              [else
+               (split-list (cdr start-list) (cons (first start-list) end-list) (sub1 n))]))
+          
+          ;(check-expect (make-list-n '(1 2 3 4) '() 2) (make-posn '(3 4) '(2 1)))
+          ;(check-expect (make-list-n '(1 2 3) '() 1) (make-posn '(2 3) '(1)))
+          ;(check-expect (make-list-n '() '() 5) (make-posn '() '()))
+          ;(check-expect (make-list-n '(1) '() 0) (make-posn '(1) '()))
+          ]
+    (split-list a-list '() (floor (/ (length a-list) 2)))))
+  
+
+(check-expect (split-list '(1 2 3 4)) (make-posn '(3 4) '(2 1)))
+(check-expect (split-list '(1 2 3)) (make-posn '(2 3) '(1)))
+(check-expect (split-list '()) (make-posn '() '()))
+(check-expect (split-list '(1)) (make-posn '(1) '()))
+
+
+
+
+
+
+
+
+
+    
